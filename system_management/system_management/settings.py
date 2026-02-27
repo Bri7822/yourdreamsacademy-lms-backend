a881af6bdfa130afd898a0306b2c3b057fce0b3d
Nkosi Bright <brightchester5@gmail.com>
2026-02-27 14:48:25 +0200

    updated secret key

--- system_management/system_management/settings.py
+++ system_management/system_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-=58_&b#zxkqqwbo1+6lf=&zvl&vbx6jkgfs^2@@dk5qbpp0@p-'
+SECRET_KEY = 'django-insecure-=58_&b#zxkqqwbo1+6lf=&zvl&vbx6jkgfs^2@@dk5qbpp0@p-'
 
 ENGINE = 'django.db.backends.postgresql'
 
